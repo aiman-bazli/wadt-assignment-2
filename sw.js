86fdb4b90c006e600fdb0d6bf9b3a77c0e8eabe5
aiman-bazli <59683532+aiman-bazli@users.noreply.github.com>
2020-04-12 17:06:00 +0800

    further pwa changes

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
-var APP_PREFIX = 'WADT_Blog'     
-var VERSION = 'version_01'
+var APP_PREFIX = 'WADT-Blog-0'     
+var VERSION = 'version_01'              
 var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [      
-  '/',                      
-  '/js/active.js',
-  '/js/bootstrap.min.js',
-  '/js/jquery-2.2.4.min.js',
-  '/js/map-active.js',
-  '/js/plugins.js',
-  '/js/plugins.js',
-  '/js/popper.js',
-  '/css/animate.css',
-  '/css/bootstrap.min.css',
-  '/css/font-awesome.min.css',
-  '/css/magnific-popup.css',
-  '/css/owl.carousel.min.css',
-  '/css/responsive.css',
-  '/style.css',
-  '/about.html',
-  '/blog.html',
-  '/index.html'
+var URLS = [                            
+  '/wadt-assignment-2/js/active.js',
+  '/wadt-assignment-2/js/bootstrap.min.js',
+  '/wadt-assignment-2/js/jquery-2.2.4.min.js',
+  '/wadt-assignment-2/js/map-active.js',
+  '/wadt-assignment-2/js/plugins.js',
+  '/wadt-assignment-2/js/plugins.js',
+  '/wadt-assignment-2/js/popper.js',
+  '/wadt-assignment-2/css/animate.css',
+  '/wadt-assignment-2/css/bootstrap.min.css',
+  '/wadt-assignment-2/css/font-awesome.min.css',
+  '/wadt-assignment-2/css/magnific-popup.css',
+  '/wadt-assignment-2/css/owl.carousel.min.css',
+  '/wadt-assignment-2/css/responsive.css',
+  '/wadt-assignment-2/style.css',
+  '/wadt-assignment-2/about.html',
+  '/wadt-assignment-2/blog.html',
+  '/wadt-assignment-2/index.html'
 ]
 
 self.addEventListener('fetch', function (e) {
@@ -29,14 +28,13 @@ self.addEventListener('fetch', function (e) {
       if (request) { 
         console.log('responding with cache : ' + e.request.url)
         return request
-      } else {      
+      } else {       
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
     })
   )
 })
-
 
 self.addEventListener('install', function (e) {
   e.waitUntil(
